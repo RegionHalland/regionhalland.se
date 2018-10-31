905b536a39dab54d39ae28b39681f23f7968ca1c
Roland <roland.hyden@regionhalland.se>
2018-10-31 09:46:48 +0100

    Uppstart nya RegionHalland.se

---
+++ web/app/themes/regionhalland/resources/views/partials/content-nav.blade.php
@@ -0,0 +1,19 @@
+@if(function_exists('get_region_halland_find_on_page'))
+	@php($myNavs = get_region_halland_find_on_page())
+	@if(isset($myNavs) && count($myNavs) > 0)
+		@php($id = uniqid())
+		<nav class="content-nav-container rh-get-sticky">
+			<div class="content-nav">
+				<h4 id="{{ $id }}">Hitta på sidan</h4>
+				<ul class="content-nav__list" itemscope itemtype="http://schema.org/ItemList" aria-labelledby="{{ $id }}">
+					@foreach ($myNavs as $myNav)
+						<li class="content-nav__item" itemprop="itemListElement">
+							<a class="content-nav__link" href="#{{ $myNav['slug'] }}" data-pointstoid="{{ $myNav['slug'] }}">{!! $myNav['content'] !!}</a>
+							<meta itemprop="position" content="{{ $loop->iteration }}" />
+						</li>
+					@endforeach
+				</ul>
+			</div>
+		</nav>
+	@endif
+@endif
---
+++ web/app/themes/regionhalland/resources/views/partials/head.blade.php
@@ -0,0 +1,9 @@
+<head>
+  	<meta charset="utf-8">
+  	<meta http-equiv="x-ua-compatible" content="ie=edge">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<link rel='stylesheet' href='../include/style/main.min.css' type='text/css' media='all' />
+  	<link rel='stylesheet' href='../include/style/application.css' type='text/css' media='all' />
+	<script type='text/javascript' src='../include/scripts/jquery.min.js?ver=4.9.8'></script>
+	<script type='text/javascript' src='../include/scripts/application.js'></script>
+</head>
