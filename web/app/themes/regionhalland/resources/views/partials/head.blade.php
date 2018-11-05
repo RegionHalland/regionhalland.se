66b74f8b79d777dfaf70304c73184cb2c1977553
Roland <roland.hyden@regionhalland.se>
2018-11-05 11:45:41 +0100

    Jquery 3.3.1

--- web/app/themes/regionhalland/resources/views/partials/head.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/head.blade.php
@@ -4,6 +4,6 @@
   	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/main.min.css' type='text/css' media='all' />
   	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/application.css' type='text/css' media='all' />
-	<script type='text/javascript' src='{!! env('WP_HOME') !!}/include/scripts/jquery.min.js?ver=4.9.8'></script>
+	<script type='text/javascript' src='{!! env('WP_HOME') !!}/include/scripts/jquery.3.3.1.min.js?ver=3.1.1'></script>
 	<script type='text/javascript' src='{!! env('WP_HOME') !!}/include/scripts/application.js'></script>
 </head>
