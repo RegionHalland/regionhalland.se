a03cfbb325a561910a3f46687a4e852c118446f1
Hydén Roland RGS IT APP <rhn014@lthalland.se>
2018-12-17 10:47:01 +0100

    Byte av design med utgång VG

--- web/app/themes/regionhalland/resources/views/layouts/app.blade.php
+++ web/app/themes/regionhalland/resources/views/layouts/app.blade.php
@@ -1,16 +1,21 @@
+<?php
+	if (!isset($_SESSION)) {
+    	session_start();
+	}
+?>	
 <!doctype html>
-<html>
-  @include('partials.head')
-  <body @php body_class() @endphp>
-    <div class="container mx-auto" style="box-shadow: 0px 0px 5px #888888;">
-        @include('partials.cookie-notice')
-        @include('partials.header')
-        <div class="col-12 flex content-stretch">
-            <div class="container col-12 mx-auto">
-                @yield('content')
-                @include('partials.footer')
-            </div>
-        </div>
-    </div>
-  </body>
+<html @php(language_attributes())>
+	@include('partials.head')
+	<body @php(body_class())>
+		
+		@include('partials.jump-to-content')
+		@include('partials.cookie-notice')
+		@include('partials.site-notices')
+
+		@include('partials.header')
+
+		@yield('content')
+
+		@include('partials.footer')
+	</body>
 </html>
--- web/app/themes/regionhalland/resources/views/partials/head.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/head.blade.php
@@ -1,9 +1,23 @@
 <head>
-  	<meta charset="utf-8">
-  	<meta http-equiv="x-ua-compatible" content="ie=edge">
-  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/main.min.css' type='text/css' media='all' />
-  	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/application.css' type='text/css' media='all' />
-	<script type='text/javascript' src='{!! env('WP_HOME') !!}/include/scripts/jquery.3.3.1.min.js?ver=3.1.1'></script>
-	<script type='text/javascript' src='{!! env('WP_HOME') !!}/include/scripts/application.js'></script>
+	<meta charset="utf-8" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	
+	<link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicons/apple-touch-icon.png')" />
+	<link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicons/favicon-32x32.png')" />
+	<link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicons/favicon-16x16.png')"/>
+	<link rel="manifest" href="@asset('images/favicons/site.webmanifest')" />
+	<link rel="mask-icon" href="@asset('images/favicons/safari-pinned-tab.svg')" color="#6ba3cb" />
+	<link rel="shortcut icon" href="@asset('images/favicons/favicon.ico')" />
+	<meta name="apple-mobile-web-app-title" content="Region Halland" />
+	<meta name="application-name" content="Region Halland" />
+	<meta name="msapplication-TileColor" content="#ffffff" />
+	<meta name="msapplication-config" content="@asset('images/favicons/browserconfig.xml')" />
+	<meta name="theme-color" content="#ffffff" />
+
+	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/main.min.css' type='text/css' media='all' />
+  	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/main.css' type='text/css' media='all' />  	
+  	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/std-style.css' type='text/css' media='all' />
+  	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/rh-main.min.css' type='text/css' media='all' />
+	<script type='text/javascript' src='{!! env('WP_HOME') !!}/include/scripts/feather.min.js'></script>
 </head>
