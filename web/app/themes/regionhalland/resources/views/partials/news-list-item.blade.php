45dc6d1a93e1c41aef2535174bd120e947d79ce9
rhn014 <Roland.Hyden@RegionHalland.se>
2019-02-13 14:18:03 +0100

    nes-list updated

--- web/app/themes/regionhalland/resources/views/partials/news-list-item.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/news-list-item.blade.php
@@ -4,9 +4,7 @@
 	</a>
 	<span class="text-base block text-grey-darkest mb-4">Publicerad: <time itemprop="datePublished" datetime="{{ $post['date'] }}">{{ $post['date'] }}</time></span>
 	<p class="text-lg text-grey-darkest leading-tight mb-4">{{ html_entity_decode(wp_trim_words($post['content'], 60, '...')) }}</p>
-	<?php var_dump($post['terms']); ?>
 		@foreach($post['terms'] as $term)
 			<a href="{{ $term['link'] }}" class="px-2 mr-2 mb-2 py-1 text-sm no-underline hover:underline focus:underline text-black bg-blue-light rounded-full inline-block">{{ $term['name'] }}</a>
-		@endforeach
-	
+		@endforeach	
 </article>
