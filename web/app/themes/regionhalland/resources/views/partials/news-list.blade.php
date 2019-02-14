ad96f4711bda6222d02219dddd76c37eb13fb407
rhn014 <Roland.Hyden@RegionHalland.se>
2019-02-14 17:18:11 +0100

    Justerat nyheter

--- web/app/themes/regionhalland/resources/views/partials/news-list.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/news-list.blade.php
@@ -12,12 +12,5 @@
 			@endforeach
 		</article>
 	@endforeach
-	@if(isset($news_category->name))
-		<a href="{{ get_post_type_archive_link(get_post_type()) }}?{{'filter[category]=' .  $news_category->slug }}" class="inline-flex no-underline text-white bg-blue-dark px-6 mt-4 items-center justify-between py-4 text-lg rounded">
-		{{ get_post_type_object(get_post_type())->labels->view_items }}
-		{{ $news_category ? 'inom ' . strtolower($news_category->name) : '' }}
-		<svg class="h-4 w-4"><use xlink:href="#chevron-right" /></svg></a>
-	@else
-		<a href="/nyheter" class="inline-flex no-underline text-white bg-blue-dark px-6 mt-4 items-center justify-between py-4 text-lg rounded">Se alla nyheter<svg class="h-4 w-4"><use xlink:href="#chevron-right" /></svg></a>
-	@endif
+	<a href="/nyheter" class="inline-flex no-underline text-white bg-blue-dark px-6 mt-4 items-center justify-between py-4 text-lg rounded">Se alla nyheter<svg class="h-4 w-4"><use xlink:href="#chevron-right" /></svg></a>
 @endif
