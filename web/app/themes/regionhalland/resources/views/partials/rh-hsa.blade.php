184722ff6f5b8aefc017d486bc6041f27e41f9c6
john.stenlund@regionhaland.se <john.stenlund@regionhaland.se>
2019-02-15 11:15:02 +0100

    hsa array

--- web/app/themes/regionhalland/resources/views/partials/rh-hsa.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/rh-hsa.blade.php
@@ -1,10 +1,20 @@
 	{{--
     HSA KONTAKTKORT
-    beroende av plugin rhkontaktkort
+    beroende av plugin regionhaland/rhkontaktkort
   --}}
 
 
  @if(function_exists('rh_hsafrontend'))
-  	{!! rh_hsafrontend(get_the_ID()) !!}
+  	@php( $rh_hak = rh_hsafrontend(get_the_ID()))
+			<div class="relative p-3 bg-grey-light">
+	           @if(!empty($rh_hak['images']))
+							 <img src="{{ $rh_hak['images'] }}" width="200" align="right" />
+							 @endif
+
+             <h2>{{ $rh_hak['name'] }}</h2>
+             <p>{{ $rh_hak['title'] }}</p>
+             <p>{{ $rh_hak['email'] }}</p>
+             <p>{{ $rh_hak['phone'] }}</p>
+	 		</div>
+
   @endif
- 
