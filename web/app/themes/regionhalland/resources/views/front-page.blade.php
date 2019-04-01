1c8955d5c83106bf358e638f648a5a71094e515d
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-01 14:15:23 +0200

    Keeping blurb images vertically centered on front page

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -28,8 +28,8 @@
 
         <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
             <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
-                <div style="height: 200px; overflow:hidden;">
-                    <img src="{!! $myBlurbs[0]['image_url'] !!}" style="width:100%">
+                <div style="height: 200px; overflow:hidden;position:relative"">
+                    <img src="{!! $myBlurbs[0]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                 </div>
                 <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                     <h3><a href="{{ $myBlurbs[0]['link_url'] }}" target="{{ $myBlurbs[0]['link_target'] }}" style="color:black">{{ $myBlurbs[0]['link_title'] }}</a></h3>
@@ -40,8 +40,8 @@
 
         <li class="left-align  col col-12 sm-col-6 md-col-6 lg-col-4">
             <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
-                <div style="height: 200px; overflow:hidden;">
-                <img src="{!! $myBlurbs[1]['image_url'] !!}" style="width:100%">
+                <div style="height: 200px; overflow:hidden;position:relative"">
+                <img src="{!! $myBlurbs[1]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                 </div>
                 <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                     <h3><a href="{{ $myBlurbs[1]['link_url'] }}" target="{{ $myBlurbs[1]['link_target'] }}" style="color:black">{{ $myBlurbs[1]['link_title'] }}</a></h3>
@@ -52,8 +52,8 @@
 
         <li class="left-align  col col-12 sm-col-6 md-col-6 lg-col-4">
             <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
-                <div style="height: 200px; overflow:hidden;">
-                <img src="{!! $myBlurbs[2]['image_url'] !!}" style="width:100%">
+                <div style="height: 200px; overflow:hidden;position:relative"">
+                <img src="{!! $myBlurbs[2]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                 </div>
                 <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                     <h3><a href="{{ $myBlurbs[2]['link_url'] }}" target="{{ $myBlurbs[2]['link_target'] }}" style="color:black">{{ $myBlurbs[2]['link_title'] }}</a></h3>
@@ -64,8 +64,8 @@
 
         <li class="left-align  col col-12 sm-col-6 md-col-6 lg-col-6">
             <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
-                <div style="height: 200px; overflow:hidden;">
-                <img src="{!! $myBlurbs[3]['image_url'] !!}" style="width:100%">
+                <div style="height: 200px; overflow:hidden;position:relative"">
+                <img src="{!! $myBlurbs[3]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                 </div>
                 <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                     <h3><a href="{{ $myBlurbs[3]['link_url'] }}" target="{{ $myBlurbs[3]['link_target'] }}" style="color:black">{{ $myBlurbs[3]['link_title'] }}</a></h3>
@@ -76,8 +76,8 @@
 
         <li class="left-align col col-12 sm-col-12 md-col-12 lg-col-6">
             <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
-                <div style="height: 200px; overflow:hidden;">
-                <img src="{!! $myBlurbs[4]['image_url'] !!}" style="width:100%">
+                <div style="height: 200px; overflow:hidden; position:relative">
+                   <img src="{!! $myBlurbs[4]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                 </div>
                 <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                     <h3><a href="{{ $myBlurbs[4]['link_url'] }}" target="{{ $myBlurbs[4]['link_target'] }}" style="color:black">{{ $myBlurbs[4]['link_title'] }}</a></h3>
