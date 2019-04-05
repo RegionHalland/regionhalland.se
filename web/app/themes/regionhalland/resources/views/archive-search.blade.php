68bdd8ee9a3718f765cdf21873ea12975717d4de
rhn014 <Roland.Hyden@RegionHalland.se>
2019-04-05 14:17:53 +0200

    search

--- web/app/themes/regionhalland/resources/views/archive-search.blade.php
+++ web/app/themes/regionhalland/resources/views/archive-search.blade.php
@@ -2,6 +2,24 @@
 
 @section('content')
 
-	söksida
+	@php($myData = get_region_halland_search_findwise_region_halland())
+
+	@php($numberOfHits = $myData['documentList']['numberOfHits'])
+
+	@php($query = $myData['query'])
+
+	@php($hitsPerPage = $myData['documentList']['pagination']['hitsPerPage'])
+
+	@php($currentPage = 1+$myData['documentList']['pagination']['offset']/$hitsPerPage)
+
+	@php($numberOfPages = ceil($numberOfHits/$hitsPerPage))
+
+	@php($arrFirst = $myData['documentList']['pagination']['firstPage'])
+
+	@php($arrPrev = $myData['documentList']['pagination']['previousPage'])
+
+	@php($arrNext = $myData['documentList']['pagination']['nextPage'])
+
+	@php($arrLast = $myData['documentList']['pagination']['lastPage'])
 	
 @endsection
