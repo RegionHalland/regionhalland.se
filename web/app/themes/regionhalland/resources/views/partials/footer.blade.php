bdb178041a7168340384b54153aa46129d3e1fa7
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-05 13:34:55 +0200

    Footer padding

--- web/app/themes/regionhalland/resources/views/partials/footer.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <div id="footer-top-placeholder" style="background-color: #C3DCC1; line-height: 1.4;">
-    <div class="clearfix center pt3" style="max-width: 1440px;">
+    <div class="clearfix center pt3 pb3" style="max-width: 1440px;">
         <div class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
             <p class="h2">Kontakta oss</p>
             <p>
