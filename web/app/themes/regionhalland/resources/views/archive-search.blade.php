e54116cb17946f7caa36b98cd4adaf0ec2395b3d
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-05 11:05:45 +0200

    Cleaning up breadcrumbs on search results

--- web/app/themes/regionhalland/resources/views/archive-search.blade.php
+++ web/app/themes/regionhalland/resources/views/archive-search.blade.php
@@ -63,14 +63,13 @@
                     </li>
                     {{-- <span>{{ $data['content'] }}</span><br><br> --}}
                         @if(function_exists('get_region_halland_breadcrumbs'))
-                            {!! getID($data['_id']) !!}
                             @php($myBreadcrumbs = get_region_halland_breadcrumbs_pages_search(getID($data['_id']), $data['title'], 'Start'))
                             @if(isset($myBreadcrumbs))
                                 @foreach ($myBreadcrumbs as $myBreadcrumb)
                                     @if ($myBreadcrumb['url'])
                                         <a href="{{ $myBreadcrumb['url'] }}">{!! $myBreadcrumb['name'] !!}</a>
                                     @else
-                                        <span>{!! $myBreadcrumb['name'] !!}</span>
+                                        <a href="{{ $data['url'] }}">{!! $myBreadcrumb['name'] !!}</a>
                                     @endif
                                 @endforeach
                             @endif
