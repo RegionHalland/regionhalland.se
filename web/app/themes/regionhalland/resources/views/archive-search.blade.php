f0d46c7d2f6487dd593b468d9639748ffe9e9645
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-05 11:14:18 +0200

    Cleaning up breadcrumbs on search results

--- web/app/themes/regionhalland/resources/views/archive-search.blade.php
+++ web/app/themes/regionhalland/resources/views/archive-search.blade.php
@@ -65,7 +65,7 @@
                             @if(isset($myBreadcrumbs))
                                 @foreach ($myBreadcrumbs as $myBreadcrumb)
                                     @if ($myBreadcrumb['url'])
-                                        <a href="{{ $myBreadcrumb['url'] }}" style="color:#378A30; text-decoration:none;">{!! $myBreadcrumb['name'] !!}</a><span style="color:#378A30"> > </span>
+                                        <a href="{{ $myBreadcrumb['url'] }}" style="color:#378A30; text-decoration:none;">{!! $myBreadcrumb['name'] !!}</a><span style="color:#378A30">  >  </span>
                                     @else
                                         <a href="{{ $data['url'] }}" style="color:#378A30; text-decoration:none;">{!! $myBreadcrumb['name'] !!}</a>
                                     @endif
