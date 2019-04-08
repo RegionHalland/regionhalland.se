4c0b3008259270374d36a18394469746c9277f00
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-08 10:02:53 +0200

    Using new blurbs on culture overview page as well

--- web/app/themes/regionhalland/resources/views/template-section-culture.blade.php
+++ web/app/themes/regionhalland/resources/views/template-section-culture.blade.php
@@ -62,16 +62,16 @@
     @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs(1571))
     @if(isset($myBlurbs))
         <div class="center" style="max-width: 1440px;">
-            <ul class="flex flex-wrap pt4 pb3 mx3" aria-label="Puffar">
+            <ul class="clearfix center mb2 mt2" style="max-width:1440px;">
                 @foreach ($myBlurbs as $blurbs)
-                    <li class="col-12 sm-col-6 md-col-4 lg-col-4 pr3">
-                        <div class="demo">
-                            <div class="rh-block">
-                                <img src="{!! $blurbs['image_url'] !!}">
-                                <div class="rh-text">
-                                    <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}">{{ $blurbs['link_title'] }}</a></h3>
-                                    <p>{{ $blurbs['post_content'] }}</p>
-                                </div>
+                    <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
+                        <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
+                            <div style="height: 200px; overflow:hidden;position:relative">
+                                <img src="{!! $blurbs['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
+                            </div>
+                            <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
+                                <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}" style="color:black;">{{ $blurbs['link_title'] }}</a></h3>
+                                <p>{{ $blurbs['post_content'] }}</p>
                             </div>
                         </div>
                     </li>
