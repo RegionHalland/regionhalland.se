7e0f6aafda3ef575f773517080dcc8833ad85c35
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-08 10:39:51 +0200

    Fixed height of nav cards on section pages for better layout

--- web/app/themes/regionhalland/resources/views/partials/section-navigation.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/section-navigation.blade.php
@@ -2,14 +2,14 @@
     <ul class="clearfix pt3 mx3 mb4" aria-label="Undersidor" style="background-color: white; border-radius: 0.4ex;box-shadow: 1px 2px 6px grey;">
         @php($page_children = get_region_halland_page_children())
         @foreach($page_children as $index => $page)
-            <li class="rh-navigation-card col col-12 sm-col-6 md-col-4 lg-col-4 px3">
+            <li class="rh-navigation-card col col-12 sm-col-6 md-col-4 lg-col-4 px3" style="height: 100px; position:relative">
                 <div class="rh-navigation-card-title">
                     <span class="rh-navigation-card-title-icon"></span>
                     <strong><a href="{{ $page->url }}" class="h3" style="text-decoration: none; color: black;">
                         {{ $page->post_title }}
                     </a></strong>
                 </div>
-                <p class="rh-navigation-card-description">
+                <p class="rh-navigation-card-description" style="overflow:hidden; color:#575757;">
                     {{ get_region_halland_acf_page_navigation_text($page->ID) }}
                 </p>
             </li>
