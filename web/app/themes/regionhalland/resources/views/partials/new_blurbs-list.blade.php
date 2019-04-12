803530bb9ad538d29df01c8e8d339ea815d52d81
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-12 10:38:29 +0200

    Trying out background color on blurbs

--- web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
@@ -5,7 +5,7 @@
         @foreach ($myBlurbs as $blurbs)
             <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
                 <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
-                    <div style="height: 200px; overflow:hidden;position:relative">
+                    <div style="height: 200px; overflow:hidden;position:relative; background-color: #d1cbc7">
                         <img src="{!! $blurbs['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
                     <div class="mx2 pt1" style="height: 18ex; overflow:hidden;">
