300123fe364a8388efb80fbc0bc2ab818405843b
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-12 10:20:44 +0200

    Revering incorrect change

--- web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
@@ -6,7 +6,7 @@
             <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
                 <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
                     <div style="height: 200px; overflow:hidden;position:relative">
-                        {!! $blurbs['image'] !!}
+                        <img src="{!! $blurbs['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
                     <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                         <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}" style="color:black;">{{ $blurbs['link_title'] }}</a></h3>
