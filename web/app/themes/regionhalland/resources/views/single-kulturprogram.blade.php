edc3c6638414d558e0aaefb1a6346afae8855d5d
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-12 11:10:59 +0200

    Moved last edit info down a bit

--- web/app/themes/regionhalland/resources/views/single-kulturprogram.blade.php
+++ web/app/themes/regionhalland/resources/views/single-kulturprogram.blade.php
@@ -19,7 +19,9 @@
 						<div class="col col-12 md-col-9 pr4">
 							<h1 class="">{{ get_the_title() }}</h1>
 							<p>{{ the_content() }}</p>
+							<p class="pt2">
 							@include('partials.author-info')
+							</p>
 						</div>
 						<div class="col col-12 md-col-3">
 							<div class="" >
