11404c7ab10c23b8dbcb9f294cef10c134543a2b
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-13 22:11:09 +0200

    Blurbs are now variable height when stacking vertically

--- web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
@@ -4,11 +4,11 @@
         <ul class="clearfix center mb2 mt2" style="max-width:1440px;">
         @foreach ($myBlurbs as $blurbs)
             <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
-                <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
+                <div class="rh-blurb mx1 my2">
                     <div style="height: 200px; overflow:hidden;position:relative;">
                         <img src="{!! $blurbs['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
-                    <div class="mx2 pt2" style="height: 20ex; overflow:hidden;">
+                    <div class="rh-blurb__description mx2 pt2">
                         <h3 style="line-height: 1.3"><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}" style="color:black;">{{ $blurbs['link_title'] }}</a></h3>
                         <p style="line-height: 1.4">{{ $blurbs['post_content'] }}</p>
                     </div>
