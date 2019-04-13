2e3b2c5d267dfec200ba4dd7ed34cfb49038eab7
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-13 22:31:47 +0200

    Reverting front page blade because of IE troubles

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -7,7 +7,7 @@
         <ul class="flex flex-wrap px3 pt3 center" aria-label="Undersidor" style="max-width: 1440px;">
         @php($first_level_pages = get_region_halland_tree_first_level())
             @foreach($first_level_pages as $index => $page)
-                <li class="rh-navigation-card left-align col-12 sm-col-6 md-col-4 lg-col-3 pr2" style="position:relative">
+                <li class="rh-navigation-card left-align col-12 sm-col-6 md-col-4 lg-col-3 pr2" style="height: 100px; position:relative">
                     <div class="rh-navigation-card-title">
                         <span class="rh-navigation-card-title-icon"></span>
                         <strong><a href="{{ $page->url }}" class="h3" style="color:black; text-decoration: none;">
@@ -27,11 +27,11 @@
     <ul class="clearfix center mb2 mt2" style="max-width:1440px;">
 
         <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
-            <div class="rh-blurb mx1 my2">
+            <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
                 <div style="height: 200px; overflow:hidden;position:relative">
                     <img src="{!! $myBlurbs[0]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                 </div>
-                <div class="mx2 pt2 rh-blurb__description">
+                <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                     <h3><a href="{{ $myBlurbs[0]['link_url'] }}" target="{{ $myBlurbs[0]['link_target'] }}" style="color:black">{{ $myBlurbs[0]['link_title'] }}</a></h3>
                     <p>{{ $myBlurbs[0]['post_content'] }}</p>
                 </div>
@@ -39,11 +39,11 @@
         </li>
 
         <li class="left-align  col col-12 sm-col-6 md-col-6 lg-col-4">
-            <div class="rh-blurb mx1 my2">
+            <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
                 <div style="height: 200px; overflow:hidden;position:relative">
                 <img src="{!! $myBlurbs[1]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                 </div>
-                <div class="rh-blurb__description mx2 pt2" >
+                <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                     <h3><a href="{{ $myBlurbs[1]['link_url'] }}" target="{{ $myBlurbs[1]['link_target'] }}" style="color:black">{{ $myBlurbs[1]['link_title'] }}</a></h3>
                     <p>{{ $myBlurbs[1]['post_content'] }}</p>
                 </div>
@@ -51,11 +51,11 @@
         </li>
 
         <li class="left-align  col col-12 sm-col-6 md-col-6 lg-col-4">
-            <div class="rh-blurb mx1 my2">
+            <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
                 <div style="height: 200px; overflow:hidden;position:relative">
                 <img src="{!! $myBlurbs[2]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                 </div>
-                <div class="mx2 pt2 rh-blurb__description">
+                <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                     <h3><a href="{{ $myBlurbs[2]['link_url'] }}" target="{{ $myBlurbs[2]['link_target'] }}" style="color:black">{{ $myBlurbs[2]['link_title'] }}</a></h3>
                     <p>{{ $myBlurbs[2]['post_content'] }}</p>
                 </div>
@@ -63,11 +63,11 @@
         </li>
 
         <li class="left-align  col col-12 sm-col-6 md-col-6 lg-col-6">
-            <div class="rh-blurb mx1 my2">
+            <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
                 <div style="height: 200px; overflow:hidden;position:relative">
                 <img src="{!! $myBlurbs[3]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                 </div>
-                <div class="rh-blurb__description mx2 pt2">
+                <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                     <h3><a href="{{ $myBlurbs[3]['link_url'] }}" target="{{ $myBlurbs[3]['link_target'] }}" style="color:black">{{ $myBlurbs[3]['link_title'] }}</a></h3>
                     <p>{{ $myBlurbs[3]['post_content'] }}</p>
                 </div>
@@ -75,11 +75,11 @@
         </li>
 
         <li class="left-align col col-12 sm-col-12 md-col-12 lg-col-6">
-            <div class="rh-blurb mx1 my2">
+            <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
                 <div style="height: 200px; overflow:hidden; position:relative">
                    <img src="{!! $myBlurbs[4]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                 </div>
-                <div class="rh-blurb__description mx2 pt2">
+                <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
                     <h3><a href="{{ $myBlurbs[4]['link_url'] }}" target="{{ $myBlurbs[4]['link_target'] }}" style="color:black">{{ $myBlurbs[4]['link_title'] }}</a></h3>
                     <p>{{ $myBlurbs[4]['post_content'] }}</p>
                 </div>
