7f4ec37757f45a19af343ba30683cb5b4d26c4db
rhn014 <Roland.Hyden@RegionHalland.se>
2019-04-26 09:21:54 +0200

    Version 1.2.0 av api-analys

--- web/app/themes/regionhalland/resources/views/partials/analys-single.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/analys-single.blade.php
@@ -1,65 +1,87 @@
 <h2 class="mb3" style="border-bottom: 2px solid #004890;">{!! $myData['name'] !!}</h2>
 <table class="rh-table-listing--alternating mb3" style="width:100%; line-height: 1.4">
+@if($myData['vas-order-code'])
 <tr>
     <td class="rh-table-listing_cell"><strong>VAS Provkod:</strong></td>
     <td class="rh-table-listing_cell">
-    	{!! $myData['vasOrderCode'] !!}<br>
+    	{!! $myData['vas-order-code'] !!}
     </td>
 </tr>
+@endif
+@if($myData['laboratory-name'])
 <tr>
     <td class="rh-table-listing_cell"><strong>Analyserande lab:</strong></td>
     <td class="rh-table-listing_cell">
-    	{!! $myData['laboratory']['name'] !!}<br>
-    	{!! $myData['laboratory']['description'] !!}<br>
+    	{!! $myData['laboratory-name'] !!}<br>
+    	{!! $myData['laboratory-description'] !!}
     </td>
 </tr>
+@endif
+@if($myData['referral-name'])
 <tr>
     <td class="rh-table-listing_cell"><strong>Remiss:</strong></td>
     <td class="rh-table-listing_cell">
-    	{!! $myData['referral']['name'] !!}<br>
-    	{!! $myData['referral']['description'] !!}<br>
-    	<a href="{!! $myData['referral']['linkUrl'] !!}" target="_blank">{!! $myData['referral']['linkText'] !!}</a><br>
+    	{!! $myData['referral-name'] !!}<br>
+    	{!! $myData['referral-description'] !!}<br>
+    	<a href="{!! $myData['referral-link-url'] !!}" target="_blank">{!! $myData['referral-link-text'] !!}</a>
     </td>
 </tr>
+@endif
+@if($myData['sampling-material-name'])
 <tr>
     <td class="rh-table-listing_cell"><strong>Provtagningsmaterial:</strong></td>
     <td class="rh-table-listing_cell">
-    	{!! $myData['samplingMaterial']['name'] !!}<br>
-    	{!! $myData['samplingMaterial']['description'] !!}<br>
+    	{!! $myData['sampling-material-name'] !!}<br>
+    	{!! $myData['sampling-material-description'] !!}
     </td>
 </tr>
+@endif
+@if($myData['unit-name'])
 <tr>
     <td class="rh-table-listing_cell"><strong>Enhet:</strong></td>
     <td class="rh-table-listing_cell">
-    	{!! $myData['unit']['name'] !!}<br>
+    	{!! $myData['unit-name'] !!}
     </td>
 </tr>
+@endif
+@if($myData['informations-text-info'])
 <tr>
     <td class="rh-table-listing_cell"><strong>Kommentar/mer info:</strong></td>
     <td class="rh-table-listing_cell">
-    	{!! $myData['informations'][0]['text'] !!}<br>
+    	{!! $myData['informations-text-info'] !!}
     </td>
 </tr>
+@endif
+@if($myData['informations-text-interval'])
 <tr>
     <td class="rh-table-listing_cell"><strong>Referensintervall:</strong></td>
     <td class="rh-table-listing_cell">
-    	{!! $myData['informations'][2]['text'] !!}<br>
+    	{!! $myData['informations-text-interval'] !!}
     </td>
 </tr>
+@endif
+@if($myData['frequency'])
 <tr>
     <td class="rh-table-listing_cell"><strong>Analysfrekvens:</strong></td>
-    <td class="rh-table-listing_cell">{!! $myData['frequency'] !!}</td>
+    <td class="rh-table-listing_cell">
+    	{!! $myData['frequency'] !!}
+    </td>
 </tr>
+@endif
+@if($myData['price'])
 <tr>
     <td class="rh-table-listing_cell"><strong>Pris:</strong></td>
-    <td class="rh-table-listing_cell">{!! $myData['price'] !!}</td>
+    <td class="rh-table-listing_cell">
+    	{!! $myData['price'] !!}
+    </td>
 </tr>
+@endif
+@if($myData['accredited-text'])
 <tr>
     <td class="rh-table-listing_cell"><strong>Ackrediterad:</strong></td>
-    @if($myData['accredited'] == 1)
-    <td class="rh-table-listing_cell">Ja</td>
-    @else
-    <td class="rh-table-listing_cell">Nej</td>
-    @endif
+    <td class="rh-table-listing_cell">
+    	{!! $myData['accredited-text'] !!}
+    </td>
 </tr>
+@endif
 </table>
