886f4628f30bb93d5de8148435f6908b52f8778e
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-04-29 14:07:43 +0200

    Improved table look on single page

--- web/app/themes/regionhalland/resources/views/partials/analys-single.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/analys-single.blade.php
@@ -18,16 +18,16 @@
 <table class="rh-table-listing--hr mb3" style="width:100%; line-height: 1.4">
 @if($myData['vas-order-code'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>VAS Provkod:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>VAS Provkod:</strong></td>
+    <td class="rh-table-listing_cell--hr">
         {!! $myData['vas-order-code'] !!}
     </td>
 </tr>
 @endif
 @if($myData['laboratory-name'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Analyserande lab:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Analyserande lab:</strong></td>
+    <td class="rh-table-listing_cell--hr">
     	{!! $myData['laboratory-name'] !!}<br>
     	{!! $myData['laboratory-description'] !!}<br>
         @if($myData['laboratory-link-url'])
@@ -38,8 +38,8 @@
 @endif
 @if($myData['referral-name'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Remiss:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Remiss:</strong></td>
+    <td class="rh-table-listing_cell--hr">
     	{!! $myData['referral-name'] !!}<br>
     	{!! $myData['referral-description'] !!}<br>
     	@if($myData['referral-link-url'])
@@ -50,8 +50,8 @@
 @endif
 @if($myData['sampling-material-name'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Provtagningsmaterial:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Provtagningsmaterial:</strong></td>
+    <td class="rh-table-listing_cell--hr">
     	{!! $myData['sampling-material-name'] !!}<br>
     	{!! $myData['sampling-material-description'] !!}<br>
         @if($myData['sampling-material-link-url'])
@@ -62,40 +62,40 @@
 @endif
 @if($myData['informations-text-provtagning'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Provtagning:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Provtagning:</strong></td>
+    <td class="rh-table-listing_cell--hr">
         {!! $myData['informations-text-provtagning'] !!}
     </td>
 </tr>
 @endif
 @if($myData['informations-text-take-care'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Omhändertagande:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Omhändertagande:</strong></td>
+    <td class="rh-table-listing_cell--hr">
         {!! $myData['informations-text-take-care'] !!}
     </td>
 </tr>
 @endif
 @if($myData['informations-text-indication'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Indikation:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Indikation:</strong></td>
+    <td class="rh-table-listing_cell--hr">
         {!! $myData['informations-text-indication'] !!}
     </td>
 </tr>
 @endif
 @if($myData['method'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Metod:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Metod:</strong></td>
+    <td class="rh-table-listing_cell--hr">
         {!! $myData['method'] !!}
     </td>
 </tr>
 @endif
 @if($myData['informations-text-info'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Kommentar/mer info:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Kommentar/mer info:</strong></td>
+    <td class="rh-table-listing_cell--hr">
     	{!! $myData['informations-text-info'] !!}
         @if($myData['links-link-url'])
             <a href="{!! $myData['links-link-url'] !!}" target="_blank">{!! $myData['links-link-text'] !!}</a>
@@ -105,64 +105,64 @@
 @endif
 @if($myData['unit-name'] && $myData['unit-name'] != 'Ingen enhet')
     <tr class="rh-table-listing__row">
-        <td class="rh-table-listing_cell"><strong>Enhet:</strong></td>
-        <td class="rh-table-listing_cell">
+        <td class="rh-table-listing_cell--hr"><strong>Enhet:</strong></td>
+        <td class="rh-table-listing_cell--hr">
             {!! $myData['unit-name'] !!}
         </td>
     </tr>
 @endif
 @if($myData['informations-text-interval'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Referensintervall:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Referensintervall:</strong></td>
+    <td class="rh-table-listing_cell--hr">
     	{!! $myData['informations-text-interval'] !!}
     </td>
 </tr>
 @endif
 @if($myData['informations-text-answer'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Svarstolkning:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Svarstolkning:</strong></td>
+    <td class="rh-table-listing_cell--hr">
         {!! $myData['informations-text-answer'] !!}
     </td>
 </tr>
 @endif
 @if($myData['frequency'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Analysfrekvens:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Analysfrekvens:</strong></td>
+    <td class="rh-table-listing_cell--hr">
     	{!! $myData['frequency'] !!}
     </td>
 </tr>
 @endif
 @if($myData['response-time'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Svarstid:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Svarstid:</strong></td>
+    <td class="rh-table-listing_cell--hr">
         {!! $myData['response-time'] !!}
     </td>
 </tr>
 @endif
 @if($myData['price'])
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Pris:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Pris:</strong></td>
+    <td class="rh-table-listing_cell--hr">
     	{!! $myData['price'] !!}
     </td>
 </tr>
 @endif
 @if($myData['accredited-text'] && $myData['accredited-value'] == 1)
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Ackrediterad:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Ackrediterad:</strong></td>
+    <td class="rh-table-listing_cell--hr">
     	{!! $myData['accredited-text'] !!}
     </td>
 </tr>
 @endif
 @if($myData['biobank-text'] && $myData['biobank-value'] == 1)
 <tr class="rh-table-listing__row">
-    <td class="rh-table-listing_cell"><strong>Biobank:</strong></td>
-    <td class="rh-table-listing_cell">
+    <td class="rh-table-listing_cell--hr"><strong>Biobank:</strong></td>
+    <td class="rh-table-listing_cell--hr">
         {!! $myData['biobank-text'] !!}
     </td>
 </tr>
