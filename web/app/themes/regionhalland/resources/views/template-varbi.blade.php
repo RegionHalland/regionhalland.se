47ca5568e0514395d096ea4f05bb437ad88222fa
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-03 10:41:05 +0200

    Bullet list on varbi

--- web/app/themes/regionhalland/resources/views/template-varbi.blade.php
+++ web/app/themes/regionhalland/resources/views/template-varbi.blade.php
@@ -64,7 +64,7 @@
 				@if(function_exists('get_region_halland_prepare_the_content'))
 					@php(get_region_halland_prepare_the_content())
 				@endif
-				<article class="rh-article">
+				<article>
 					<p><strong>{{ get_region_halland_acf_page_ingress() }}</strong></p>
 					{!! the_content() !!}
 				</article>
