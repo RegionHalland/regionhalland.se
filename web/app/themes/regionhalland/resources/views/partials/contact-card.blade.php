af75ddaa753b0666328b4798c16049c86a115859
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-03 09:48:10 +0200

    Using border bottom instead of buggy hr

--- web/app/themes/regionhalland/resources/views/partials/contact-card.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/contact-card.blade.php
@@ -3,45 +3,51 @@
 @php($myEnhet = get_region_halland_acf_page_personer_enheter_enhet())
 
 @if($myPerson)
-@php($myPersonData = get_region_halland_api_personer_enheter(1, $myPerson, ""))
+    @php($myPersonData = get_region_halland_api_personer_enheter(1, $myPerson, ""))
     @if($myPersonData)
         <div class="rh-personal-info pt2 mb2">
-            <div class="rh-box-info-alone">
+            <div class="rh-box-info-alone pb2 mb2" style="border-bottom: 2px solid #004890;">
                 <p><strong>{{ $myPersonData['firstname'] }} {{ $myPersonData['lastname'] }}</strong></p>
                 <span class="rh-personal-info-text">{{ $myPersonData['jobtitle'] }}</span>
-                <hr class="rh-personal-info-hr">
             </div>
         </div>
     @endif
 @endif
 
 @if($myEnhet)
-@php($myEnhetData = get_region_halland_api_personer_enheter(2, "", $myEnhet))
+    @php($myEnhetData = get_region_halland_api_personer_enheter(2, "", $myEnhet))
     @if($myEnhetData)
         <div class="rh-unit-info pt2">
 
-            <p><strong>{{ $myEnhetData['enhet_name'] }}</strong></p>
-            <hr class="rh-unit-info-title-hr">
-            <p class="rh-unit-info-text rh-unit-info-text-inline"><strong>Telefontider:</strong>
+            <div class="mb2 pb2" style="border-bottom: 2px solid #004890;">
+                <strong>
+                    {{ $myEnhetData['enhet_name'] }}
+                </strong>
+            </div>
+
+            <p class="rh-unit-info-text rh-unit-info-text-inline">
+                <strong>
+                    Telefontider:
+                </strong>
                 @foreach ($myEnhetData['enhet_telephones'] as $phones)
                     {{ $phones['landsnummer'] }} {{ $phones['riktnummer'] }} {{ $phones['telefonnummer'] }}<br>
-                    <ul>
-                        @foreach ($phones['telefontider'] as $tider)
-                            <li>
-                                {{ $tider['day'] }} {{ $tider['openingTime'] }} - {{ $tider['closingTime'] }}
-                            </li>
-                        @endforeach
-                    </ul>
+            <ul>
+                @foreach ($phones['telefontider'] as $tider)
+                    <li>
+                        {{ $tider['day'] }} {{ $tider['openingTime'] }} - {{ $tider['closingTime'] }}
+                    </li>
                 @endforeach
+            </ul>
+            @endforeach
             </p>
             <p class="rh-unit-info-text"><strong>Öppettider:</strong>
-                <ul>
-                    @foreach ($myEnhetData['visiting_hours'] as $visiting)
-                        <li>
-                            {{ $visiting['day'] }} {{ $visiting['openingTime'] }} - {{ $visiting['closingTime'] }}
-                        </li>
-                    @endforeach
-                </ul>
+            <ul>
+                @foreach ($myEnhetData['visiting_hours'] as $visiting)
+                    <li>
+                        {{ $visiting['day'] }} {{ $visiting['openingTime'] }} - {{ $visiting['closingTime'] }}
+                    </li>
+                @endforeach
+            </ul>
             </p>
             <p class="rh-unit-info-text"><strong>Besöksadress: </strong>{{ $myEnhetData['address_street'] }}<br>
                 {{ $myEnhetData['address_postcode'] }} {{ $myEnhetData['address_city'] }}<br>
