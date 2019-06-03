a92ad9ba4513e5eb992c37d55ac0239ed4a7a6d3
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-06-03 09:37:31 +0200

    Trying out some padding and margin

--- web/app/themes/regionhalland/resources/views/partials/contact-card.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/contact-card.blade.php
@@ -6,7 +6,7 @@
     @php($myPersonData = get_region_halland_api_personer_enheter(1, $myPerson, ""))
     @if($myPersonData)
         <div class="rh-personal-info pt2 mb2">
-            <div class="rh-box-info-alone" style="border-bottom: 2px solid #004890;">
+            <div class="rh-box-info-alone pb2 mb2" style="border-bottom: 2px solid #004890;">
                 <p><strong>{{ $myPersonData['firstname'] }} {{ $myPersonData['lastname'] }}</strong></p>
                 <span class="rh-personal-info-text">{{ $myPersonData['jobtitle'] }}</span>
             </div>
