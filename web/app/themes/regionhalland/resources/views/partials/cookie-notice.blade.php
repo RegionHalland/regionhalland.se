309d44026a87810187441a6a13a6c1b0529cc837
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-22 17:14:22 +0200

    Containing all content in semantic landmark tags

--- web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
@@ -1,11 +1,13 @@
 @php($checkCookieNotice = check_region_halland_cookie_notice())
 @if ($checkCookieNotice == false)
 	@php($cookie_notice = get_region_halland_cookie_notice())
-	<div class="rh-cookie" style="background-color: rgba(195,220,193,0.5);">
-		<span class="rh-cookie-icon"></span>
-		<p class="rh-cookie-description">
-            {!! $cookie_notice['message'] !!}
-		</p>
-		<button id="cookie-consent" style="background-color: #378A30;width:13ex;" class="rh-button rh-button--primary cookie-button" aria-label="Acceptera cookies och dölj meddelandet" role="button">{!! $cookie_notice['button'] !!}</button>
-	</div>
+	<header>
+		<div class="rh-cookie" style="background-color: rgba(195,220,193,0.5);">
+			<span class="rh-cookie-icon"></span>
+			<p class="rh-cookie-description">
+				{!! $cookie_notice['message'] !!}
+			</p>
+			<button id="cookie-consent" style="background-color: #378A30;width:13ex;" class="rh-button rh-button--primary cookie-button" aria-label="Acceptera cookies och dölj meddelandet" role="button">{!! $cookie_notice['button'] !!}</button>
+		</div>
+	</header>
 @endif
--- web/app/themes/regionhalland/resources/views/template-section-culture.blade.php
+++ web/app/themes/regionhalland/resources/views/template-section-culture.blade.php
@@ -57,29 +57,29 @@
             @endif
         @endif
 
+        @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs(1571))
+        @if(isset($myBlurbs))
+            <div class="center" style="max-width: 1440px;">
+                <ul class="clearfix center mb2 mt2" style="max-width:1440px;">
+                    @foreach ($myBlurbs as $blurbs)
+                        <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
+                            <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
+                                <div style="height: 200px; overflow:hidden;position:relative">
+                                    <img src="{!! $blurbs['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
+                                </div>
+                                <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
+                                    <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}" style="color:black;">{{ $blurbs['link_title'] }}</a></h3>
+                                    <p>{{ $blurbs['post_content'] }}</p>
+                                </div>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
     </main>
 
     @endwhile
-
-    @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs(1571))
-    @if(isset($myBlurbs))
-        <div class="center" style="max-width: 1440px;">
-            <ul class="clearfix center mb2 mt2" style="max-width:1440px;">
-                @foreach ($myBlurbs as $blurbs)
-                    <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
-                        <div class="mx1 my2" style="border-bottom: 4px solid #C3DCC1; border-left: 1px solid #E4E4E4; border-right: 1px solid #E4E4E4;">
-                            <div style="height: 200px; overflow:hidden;position:relative">
-                                <img src="{!! $blurbs['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
-                            </div>
-                            <div class="mx2 pt2" style="height: 18ex; overflow:hidden;">
-                                <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}" style="color:black;">{{ $blurbs['link_title'] }}</a></h3>
-                                <p>{{ $blurbs['post_content'] }}</p>
-                            </div>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 
 @endsection
