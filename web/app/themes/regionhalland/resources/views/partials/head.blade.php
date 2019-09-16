6804e5ff14bd78c6eb2eb544a85b3b5932319478
rhn014 <Roland.Hyden@RegionHalland.se>
2019-09-16 16:11:57 +0200

    test av 1.1.0-5.1.0 av css

--- web/app/themes/regionhalland/resources/views/partials/head.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/head.blade.php
@@ -17,7 +17,7 @@
 	<title>{{ get_region_halland_meta_data_region_halland_title() }}</title>
 	<link rel="stylesheet" type="text/css" href="{!! env('WP_HOME') !!}/include/alicdn/font_o5hd5vvqpoqiwwmi_190529.css">
 	{{-- <link href="{!! env('WP_HOME') !!}/styleguide4.1.0/css/components.css" rel="stylesheet"/> --}}
-	<link rel="stylesheet" href="{!! env('WP_HOME') !!}/include/style/regionhalland.1.0.0-5.1.0.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="{!! env('WP_HOME') !!}/include/style/regionhalland.1.1.0-5.1.0.css" type="text/css" media="all" />
 	<link rel="stylesheet" href="{!! env('WP_HOME') !!}/include/style/development.css" type="text/css" media="all" />
 	{{-- <link href="https://unpkg.com/basscss@8.0.2/css/basscss.min.css" rel="stylesheet" /> --}}
 	<style>
