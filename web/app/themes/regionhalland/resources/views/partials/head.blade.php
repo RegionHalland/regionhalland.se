94bc1537066c61976b6823d28fa6b00ce698aab8
rhn014 <Roland.Hyden@RegionHalland.se>
2019-09-25 09:31:53 +0200

    meta-taggar + versionshanterad development.css

--- web/app/themes/regionhalland/resources/views/partials/head.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/head.blade.php
@@ -2,12 +2,8 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="x-ua-compatible" content="ie=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-	<link rel="apple-touch-icon" sizes="180x180" href="{!! env('WP_HOME') !!}/include/favicons/apple-touch-icon.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="{!! env('WP_HOME') !!}/include/favicons/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="{!! env('WP_HOME') !!}/include/favicons/favicon-16x16.png"/>
-	<link rel="manifest" href="{!! env('WP_HOME') !!}/include/favicons/site.webmanifest" />
-	<link rel="mask-icon" href="{!! env('WP_HOME') !!}/include/favicons/safari-pinned-tab.svg" color="#6ba3cb" />
-	<link rel="shortcut icon" href="{!! env('WP_HOME') !!}/include/favicons/favicon.ico" />
+	<meta http-equiv="Content-Security-Policy" content="script-src 'self'">
+	<meta name="referrer" content="no-referrer">
 	<meta name="apple-mobile-web-app-title" content="Region Halland" />
 	<meta name="application-name" content="Region Halland" />
 	<meta name="msapplication-TileColor" content="#ffffff" />
@@ -15,11 +11,15 @@
 	<meta name="theme-color" content="#ffffff" />
 	<meta name="description" content="{{ get_region_halland_meta_data_region_halland_description() }}" />
 	<title>{{ get_region_halland_meta_data_region_halland_title() }}</title>
+	<link rel="apple-touch-icon" sizes="180x180" href="{!! env('WP_HOME') !!}/include/favicons/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="{!! env('WP_HOME') !!}/include/favicons/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="{!! env('WP_HOME') !!}/include/favicons/favicon-16x16.png"/>
+	<link rel="manifest" href="{!! env('WP_HOME') !!}/include/favicons/site.webmanifest" />
+	<link rel="mask-icon" href="{!! env('WP_HOME') !!}/include/favicons/safari-pinned-tab.svg" color="#6ba3cb" />
+	<link rel="shortcut icon" href="{!! env('WP_HOME') !!}/include/favicons/favicon.ico" />
 	<link rel="stylesheet" type="text/css" href="{!! env('WP_HOME') !!}/include/alicdn/font_o5hd5vvqpoqiwwmi_190529.css">
-	{{-- <link href="{!! env('WP_HOME') !!}/styleguide4.1.0/css/components.css" rel="stylesheet"/> --}}
 	<link rel="stylesheet" href="{!! env('WP_HOME') !!}/include/style/regionhalland.1.2.1-5.1.0.css" type="text/css" media="all" />
-	<link rel="stylesheet" href="{!! env('WP_HOME') !!}/include/style/development.css" type="text/css" media="all" />
-	{{-- <link href="https://unpkg.com/basscss@8.0.2/css/basscss.min.css" rel="stylesheet" /> --}}
+	<link rel="stylesheet" href="{!! env('WP_HOME') !!}/include/style/development.1.0.0.css" type="text/css" media="all" />
 	<style>
 		.rh-article ul {
 			list-style-type: disc;
